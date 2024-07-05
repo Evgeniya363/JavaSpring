77334769c8b262062e80590be960aecb7c5ab3f6
Evgeniya363 <evavong363@gmail.ru>
2024-07-05 09:21:53 +0300

    Добавляем работу к 2-му семинару

---
+++ lesson2/src/main/java/ru/gb/spring/lesson2/Ticket.java
@@ -0,0 +1,19 @@
+package ru.gb.spring.lesson2;
+
+import java.time.LocalDateTime;
+
+public class Ticket {
+    String number;
+    LocalDateTime createAt;
+
+    public Ticket(String number) {
+        this.number = number;
+        this.createAt = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return number + '\'' +
+                ", от " + createAt;
+    }
+}
